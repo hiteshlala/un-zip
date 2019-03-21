c47e0264650e79d94a6336e84c27f5526b1d3a5f
hiteshlala <hiteshlala@gmail.com>
2019-03-20 21:35:17 -0700

    Reorganize code Add first successful unzip >4GB

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
 /*
+
+This is a demo script to unzip a file that uses Deflate algorithm.
+
+
+Resouce reading:
+
 https://en.wikipedia.org/wiki/Zip_(file_format)
 https://stackoverflow.com/questions/10308110/simplest-way-to-download-and-unzip-files-in-node-js-cross-platform
 https://stackoverflow.com/questions/11611950/unzip-a-zip-file-using-zlib
@@ -7,222 +13,81 @@ https://stackoverflow.com/questions/4802097/how-does-one-find-the-start-of-the-c
 
 */
 
-const fs = require( 'fs' );
-const os = require( 'os' );
-const path = require( 'path' );
-const zlib = require( 'zlib' );
-const tools = require( './tools' );
 
+const fs = require( 'fs' );
+const path = require( 'path' );
+const t = require( './tools' );
+
+
+// const source =  path.resolve( './tvgbackup_5c8d5fe24687b7000505f2d1.zip');
+// const source =  path.resolve('./testzip.zip'); // 42KB true
+const source = path.resolve( './Notredame.tvg' );
+// const source = 'E:/Data/Notredame.tvg';
 // const source = 'E:/Data/SharedData.tvg'; // 36MB true
-// const source = 'E:/Data/Notredame.tvg'; // 1.9GB true
-// const source = 'E:/Data/Monticello TruView.tvg'; // 8.4GB false
 // const source = 'E:/Data/LeicaShowroom 1.tvg'; // 4.2GB false
-const source = os.platform() === 'win32' ? 'C:/dev/testrepo/zippStuff/testzip.zip': path.resolve('./testzip.zip'); // 42KB true
+// const source = 'E:/Data/Monticello TruView.tvg'; // 8.4GB false
+
 
 const out =  path.resolve( './junk' );
-tools.mkdir( out );
-
-
-function readUInt16( buf, i ) {
-  return os.endianness() === 'LE' ? buf.readUInt16LE( i ) : buf.readUInt16BE( i );
-}
-
-function readUInt32( buf, i ) {
-  return os.endianness() === 'LE' ? buf.readUInt32LE( i ) : buf.readUInt32BE( i );
-}
-
-/**
- * Finds the End of Central Directory record
- * @param {Buffer} buf 
- */
-function findEOCD( buf ) {
-  let eocd = 0x06054b50;
-  for ( let i = 0; i < buf.length - 4; i++ ) {
-    let a = readUInt32( buf, i );
-    if ( a === eocd ) {
-      const data = { 
-        index: i, 
-        found: true,
-        diskNo: readUInt16( buf, i + 4 ),
-        diskCDStarts: readUInt16( buf, i + 6 ),
-        numCDRecOnDisk: readUInt16( buf, i + 8 ),
-        totalCDRecords: readUInt16( buf, i + 10 ),
-        sizeCDRecord: readUInt32( buf, i + 12 ),
-        startCD: readUInt32( buf, i + 16 ),
-        commentLen: readUInt16( buf, i + 20 )
-      };
-      data.comment =  buf.toString( 'utf8', i + 22, i + data.commentLen );
-      return data;
-    }
-  }
-  return { found: false }
-}
-
-/**
- * Reads the Central Directory Header located at i
- * There is one entry for each file in the zip archive
- * @param {fs.filehandle} fd 
- * @param {End of Central Directory} data 
- * @param {Number} i start of Central Directory Header
- */
-function readCDHeaderSignature( fd, data, i ) {
-  i = i || 0;
-  const expected = 0x02014b50;
-  const toRead = data.sizeCDRecord;
-  const buf = Buffer.alloc( toRead, 0 );
-  fs.readSync( fd, buf, 0, toRead, data.startCD );
-  const found =  readUInt32( buf, 0 );
-  if ( true ) { //found === expected ) {
-    const data = {
-      found: found === expected,
-      vMade: readUInt16( buf, i + 4 ),
-      vMinReq: readUInt16( buf, i + 6 ),
-      genPurFlag: readUInt16( buf, i + 8 ),
-      comprMeth: readUInt16( buf, i + 10 ),
-      lastModTime: readUInt16( buf, i + 12 ),
-      lastModDate: readUInt16( buf, i + 14 ),
-      crc32: readUInt32( buf, i + 16 ),
-      compressedSize: readUInt32( buf, i + 20 ),
-      unCompressedSize: readUInt32( buf, i + 24 ),
-      fNameLen: readUInt16( buf, i + 28 ),
-      extraFieldLen: readUInt16( buf, i +  30 ),
-      fCommentLen: readUInt16( buf, i + 32 ),
-      diskNoStart: readUInt16( buf, i + 34 ),
-      internalFileAttr: readUInt16( buf, i + 36 ),
-      externalFileAttr: readUInt32( buf, i + 38 ),
-      offsetStart: readUInt32( buf, i + 42 ),
-    }
-    data.fName = buf.toString( 'utf8', i + 46, i + 46 + data.fNameLen );
-    data.extraField = buf.toString( 'utf8', i + 46 + data.fNameLen, i + 46 + data.fNameLen + data.extraFieldLen );
-    data.comment = buf.toString( 'utf8', i + 46 + data.fNameLen + data.extraFieldLen, i + 46 + data.fNameLen + data.extraFieldLen + data.fCommentLen );
-    data.length = i + 46 + data.fNameLen + data.extraFieldLen + data.fCommentLen;
-    return data;
-  }
-  return { found: false };
-}
-
-function readLocalFileHeader( fd, i ) {
-  const expected = 0x04034b50;
-  const toRead = 1024;
-  const buf = Buffer.alloc( toRead, 0 );
-  fs.readSync( fd, buf, 0, toRead, i );
-  const found =  readUInt32( buf, 0 );
-  if ( true ) { //found === expected ) {
-    const data = {
-      found: found === expected,
-      vMinReq: readUInt16( buf, 4 ),
-      genPurFlag: readUInt16( buf, 6 ),
-      comprMeth: readUInt16( buf, 8 ),
-      lastModTime: readUInt16( buf, 10 ),
-      lastModDate: readUInt16( buf, 12 ),
-      crc32: readUInt32( buf, 14 ),
-      compressedSize: readUInt32( buf, 18 ),
-      unCompressedSize: readUInt32( buf, 22 ),
-      fNameLen: readUInt16( buf, 26 ),
-      extraFieldLen: readUInt16( buf,  28 ),
-    }
-    data.fName = buf.toString( 'utf8', 30, 30 + data.fNameLen );
-    data.extraField = buf.toString( 'utf8', 30 + data.fNameLen, 30 + data.fNameLen + data.extraFieldLen );
-    data.length = 30 + data.fNameLen + data.extraFieldLen;
-    return data;
-  }
-  return { found: false };
-}
-
-async function unzip( fd, info ) {
-  const outfilepath = path.resolve( out, info.fName );
-  if ( info.compressedSize == 0 ) {
-    tools.mkdir( outfilepath );
-    return;
-  }
-
-  const dir = path.dirname( outfilepath );
-  tools.mkdir( dir );
-
-  const data = readLocalFileHeader( fd, info.offsetStart );
-  console.log( '\nLocal File Header\n', data, '\n' );
-
-  // /*
-  try {
-    const prom = async () => new Promise(( resolve, reject) => {
-      console.log( Date.now() )
-      // /*
-      const outfile = fs.createWriteStream( outfilepath );
-      const infile = fs.createReadStream( '', {
-        fd: fd,
-        start: data.length + info.offsetStart,
-        end: data.length + info.offsetStart + info.compressedSize,
-        encoding: null
-      });
-      // const unzip = zlib.createUnzip();
-      const unzip = zlib.createInflateRaw();
-      unzip.on( 'error', ( e ) => {
-        console.log( 'unzip error', info.fName, e.message )
-        console.log( 'unzip error', e );
-        reject( e );
-      });
-      unzip.on( 'close', () => { resolve(); });
-      infile.pipe( unzip ).pipe( outfile );
-      outfile.on( 'close', () => { resolve(); });
-      outfile.on( 'error', (e) => { 
-        console.log( 'outfile error', e );
-        reject(e); 
-      });
-      // */
-    });
-    await prom();
-    return;
-  }
-  catch( e ) {
-    console.log( 'error in try catch ', e );
-    return;
-  }
-  // */
-  
-}
+t.mkdir( out );
 
 
 async function start () {
 
   const fd = fs.openSync( source, 'r' );
   const stat = fs.statSync( source );
-  console.log( stat );
   const toRead = 1024;
-  const buf = Buffer.alloc( toRead, 0 );
-  fs.readSync( fd, buf, 0, toRead, stat.size - toRead );
-  console.log( buf )
-  const result = findEOCD( buf );
-  console.log( '\nEnd Of Central Directory:\n', result, '\n' );
 
-  if ( result.found ) {
-    let CDHeader;
-    try {
-      for ( let i = 0; i < result.numCDRecOnDisk; i++ ) {
-        const index = CDHeader ? CDHeader.length : 0;
-        CDHeader = readCDHeaderSignature( fd,  result, index );
-        console.log( '\nCentral Header:', CDHeader, '\n' );
-        // await unzip( fd, CDHeader );
-        await unzip( fs.openSync( source, 'r'), CDHeader );
+  let buf = Buffer.alloc( toRead, 0 );
+  fs.readSync( fd, buf, 0, toRead, stat.size - toRead );
+  
+  const ecdr = t.readECDR( buf );
+  console.log( '\nECDR:\n', ecdr, '\n' );
+
+  const zecdl = t.readZECDL( buf );
+  console.log( '\nZECDL:\n', zecdl, '\n' );
+
+  const zecdr = zecdl.found && t.readZECDR( fd, zecdl.startZip64CD );
+  zecdl.found && console.log( 'ZECDR', zecdr );
+
+  let records = 0;
+
+  if ( ecdr.found && !zecdl.found ) {
+    const toRead = ecdr.sizeCDRecord;
+    const start = ecdr.startCD;
+    records = ecdr.numCDRecOnDisk;
+    buf = Buffer.alloc( toRead, 0 );
+    fs.readSync( fd, buf, 0, toRead, start );
+  }
+  else if ( zecdl.found ) {
+    const toRead = zecdr.sizeCDRecord;
+    const start = zecdr.startCD;
+    records = zecdr.numCDRecOnDisk;
+    buf = Buffer.alloc( toRead, 0 );
+    fs.readSync( fd, buf, 0, toRead, start );
+  }
+
+  let index = 0;
+  let cdh;
+  let count = 0;
+  for ( let i = 0; i < records; i++ ) {
+    index = cdh ? cdh.length + index : 0;
+    cdh = t.readCDH( buf, index );
+    console.log( '\nCDH:', cdh, '\n' );
+    const lfh = t.readLFH( fd, cdh.offsetStart )
+    console.log( '\nLFH:\n', lfh );
+    if ( lfh.found ) {
+      count++;
+      try {
+        await t.inflate( fs.openSync( source, 'r'), cdh, lfh, out );
+      }
+      catch( e ) {
+        console.log( 'in for loop', e );
       }
     }
-    catch( e ) {
-      console.log( 'in for loop', e );
-    }
   }
+  console.log( 'Records:', records, 'Found:', count );
+  
 }
- start();
 
-/* trying to understand little and big endian
-
-const buf = (new Uint8Array([1,2,3,4])).buffer;
-console.log( 'buffer:', buf )
-
-const isLE = (new Uint32Array((new Uint8Array([1,2,3,4])).buffer))[0] === 0x04030201;
-console.log( 'isLE', isLE )
-
-const isBE = (new Uint32Array((new Uint8Array([4,3,2,1])).buffer))[0] === 0x04030201;
-console.log( 'isBE', isBE )
-
-console.log( 'os.isLE', os.endianness() )
-
-*/
+start();
--- tools.js
+++ tools.js
@@ -1,5 +1,7 @@
 const fs = require( 'fs' );
 const path = require( 'path' );
+const os = require( 'os' );
+const zlib = require( 'zlib' );
 
 /**
  * Make directory structure recursively
@@ -41,7 +43,232 @@ function isdir( p ) {
   return /\/$/.test( p );
 }
 
+
+// I think that all the data is stored as LE fromat
+function readUInt16( buf, i ) {
+  // return os.endianness() === 'LE' ? buf.readUInt16LE( i ) : buf.readUInt16BE( i );
+  return buf.readUInt16LE( i );
+}
+
+function readUInt32( buf, i ) {
+  // return os.endianness() === 'LE' ? buf.readUInt32LE( i ) : buf.readUInt32BE( i );
+  return buf.readUInt32LE( i );
+}
+
+function readUInt64( buf, i ) {
+  const lsb = buf.readUInt32LE( i );
+  const msb = buf.readUInt32LE( i + 32/8 );
+  // return lsb + ( msb << 32 );  for some reason bit shifting more than 32 bits does not work - I think the word length is exceeded
+  // console.log( 'readUINt64', lsb + (msb<<32), msb*Math.pow(2,32) + lsb );
+  return msb * ( Math.pow( 2, 32 ) ) + lsb;
+
+}
+
+/**
+ * Finds the End of Central Directory record
+ * @param {Buffer} buf buffer containing part of the zip archive
+ */
+function readECDR( buf ) {
+  let ecdrheader = 0x06054b50;
+  for ( let i = 0; i < buf.length - 4; i++ ) {
+    let a = readUInt32( buf, i );
+    if ( a === ecdrheader ) {
+      const data = { 
+        found: true,
+        diskNo: readUInt16( buf, i + 4 ),
+        diskCDStarts: readUInt16( buf, i + 6 ),
+        numCDRecOnDisk: readUInt16( buf, i + 8 ),
+        totalCDRecords: readUInt16( buf, i + 10 ),
+        sizeCDRecord: readUInt32( buf, i + 12 ),
+        startCD: readUInt32( buf, i + 16 ),
+        commentLen: readUInt16( buf, i + 20 )
+      };
+      data.comment =  buf.toString( 'utf8', i + 22, i + data.commentLen );
+      return data;
+    }
+  }
+  return { found: false }
+}
+
+
+function readZECDL( buf ) {
+  let eocd = 0x07064b50;
+  for ( let i = 0; i < buf.length - 4; i++ ) {
+    let a = readUInt32( buf, i );
+    if ( a === eocd ) {
+      const data = { 
+        found: true,
+        diskCDStarts: readUInt16( buf, i + 4 ),
+        startZip64CD: readUInt64( buf, i + 8 ),
+        totNumDisks: readUInt32( buf, i + 16 ),
+      };
+      return data;
+    }
+  }
+  return { found: false }
+}
+
+/**
+ * Reads the Central Directory Header located at i
+ * There is one entry for each file in the zip archive
+ * @param {fs.filehandle} fd 
+ * @param {End of Central Directory} info 
+ * @param {Number} i start of this Central Directory Header record
+ */
+function readCDH( buf, i ) {
+  i = i || 0;
+  const expected = 0x02014b50;
+  const found = readUInt32( buf, 0 );
+  if ( found === expected ) {
+    const data = {
+      found: found === expected,
+      vMade: readUInt16( buf, i + 4 ),
+      vMinReq: readUInt16( buf, i + 6 ),
+      genPurFlag: readUInt16( buf, i + 8 ),
+      comprMeth: readUInt16( buf, i + 10 ),
+      lastModTime: readUInt16( buf, i + 12 ),
+      lastModDate: readUInt16( buf, i + 14 ),
+      crc32: readUInt32( buf, i + 16 ),
+      compressedSize: readUInt32( buf, i + 20 ),
+      unCompressedSize: readUInt32( buf, i + 24 ),
+      fNameLen: readUInt16( buf, i + 28 ),
+      extraFieldLen: readUInt16( buf, i +  30 ),
+      fCommentLen: readUInt16( buf, i + 32 ),
+      diskNoStart: readUInt16( buf, i + 34 ),
+      internalFileAttr: readUInt16( buf, i + 36 ),
+      externalFileAttr: readUInt32( buf, i + 38 ),
+      offsetStart: readUInt32( buf, i + 42 ),
+    }
+    data.fName = buf.toString( 'utf8', i + 46, i + 46 + data.fNameLen );
+    data.extraField = buf.toString( 'utf8', i + 46 + data.fNameLen, i + 46 + data.fNameLen + data.extraFieldLen );
+    data.comment = buf.toString( 'utf8', i + 46 + data.fNameLen + data.extraFieldLen, i + 46 + data.fNameLen + data.extraFieldLen + data.fCommentLen );
+    data.length = 46 + data.fNameLen + data.extraFieldLen + data.fCommentLen;
+    return data;
+  }
+  return { found: false };
+}
+
+function readZECDR( fd, i ) {
+  i = i || 0;
+  const expected = 0x06064b50;
+  let toRead = 12;
+
+  let data = {};
+  
+  let buf = Buffer.alloc( toRead, 0 );
+  fs.readSync( fd, buf, 0, toRead, i );
+  let sig = readUInt32( buf, 0 );
+
+  data.found = sig === expected;
+  if ( data.found ) {
+    data.sizeZEOCD = readUInt64( buf, 4 );
+    toRead = data.sizeZEOCD + 12;
+    buf = Buffer.alloc( toRead, 0 );
+    fs.readSync( fd, buf, 0, toRead, i );
+    
+    data = {
+      ...data,
+      vMade: readUInt16( buf, 12 ),
+      vMinReq: readUInt16( buf, 14 ),
+      diskNo: readUInt32( buf, 16 ),
+      diskNoStart: readUInt32( buf, 20 ),
+      numCDRecOnDisk: readUInt64( buf, 24 ),
+      totalCDRecords: readUInt64( buf, 32 ),
+      sizeCDRecord: readUInt64( buf, 40 ),
+      startCD: readUInt64( buf, 48 ),
+    };
+    data.extraField = buf.toString( 'utf8', 56,  56 + data.sizeZEOCD - 12 );
+  }
+
+  return data;
+}
+
+function readLFH( fd, i ) {
+  const expected = 0x04034b50;
+  const toRead = 1024;
+  const buf = Buffer.alloc( toRead, 0 );
+  fs.readSync( fd, buf, 0, toRead, i );
+  const found =  readUInt32( buf, 0 );
+  if ( found === expected ) {
+    const data = {
+      found: found === expected,
+      vMinReq: readUInt16( buf, 4 ),
+      genPurFlag: readUInt16( buf, 6 ),
+      comprMeth: readUInt16( buf, 8 ),
+      lastModTime: readUInt16( buf, 10 ),
+      lastModDate: readUInt16( buf, 12 ),
+      crc32: readUInt32( buf, 14 ),
+      compressedSize: readUInt32( buf, 18 ),
+      unCompressedSize: readUInt32( buf, 22 ),
+      fNameLen: readUInt16( buf, 26 ),
+      extraFieldLen: readUInt16( buf,  28 ),
+    }
+    data.fName = buf.toString( 'utf8', 30, 30 + data.fNameLen );
+    data.extraField = buf.toString( 'utf8', 30 + data.fNameLen, 30 + data.fNameLen + data.extraFieldLen );
+    data.length = 30 + data.fNameLen + data.extraFieldLen;
+    return data;
+  }
+  return { found: false };
+}
+
+
+async function inflate( fd, cdh, lfh, out ) {
+  const outfilepath = path.resolve( out, cdh.fName );
+  if ( cdh.compressedSize == 0 ) {
+    return;
+  }
+
+  const dir = path.dirname( outfilepath );
+  mkdir( dir );
+
+  // /*
+  try {
+    const prom = async () => new Promise(( resolve, reject) => {
+      // /*
+      const outfile = fs.createWriteStream( outfilepath );
+      const infile = fs.createReadStream( '', {
+        fd: fd,
+        start: lfh.length + cdh.offsetStart,
+        end: lfh.length + cdh.offsetStart + cdh.compressedSize,
+        encoding: null
+      });
+      // const unzip = zlib.createUnzip();
+      const unzip = zlib.createInflateRaw();
+      unzip.on( 'error', ( e ) => {
+        console.log( 'unzip error', cdh.fName, e.message )
+        console.log( 'unzip error', e );
+        reject( e );
+      });
+      unzip.on( 'close', () => { resolve(); });
+      infile.pipe( unzip ).pipe( outfile );
+      outfile.on( 'close', () => { resolve(); });
+      outfile.on( 'error', (e) => { 
+        console.log( 'outfile error', e );
+        reject(e); 
+      });
+      // */
+    });
+
+    return await prom();
+  }
+  catch( e ) {
+    console.log( 'error in try catch ', e );
+    return e;
+  }
+  // */
+  
+}
+
+
+
 module.exports = {
   mkdir,
   isdir,
+  readCDH,
+  readECDR,
+  readZECDL,
+  readZECDR,
+  readLFH,
+  inflate
+
 };
